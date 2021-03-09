131435cf84d83bd0e901f063e1b6dfda8b19d4a7
范轶洁 <fanyjb@digitalchina.com>
2021-03-09 17:00:26 +0800

    proxy 配置

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  proxy: {
+    prefix: "/api",
+    url: "http://appmall.digitalchina.com",
+  },
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
